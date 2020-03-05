fc808b795804505905e6f82f8d1c7d949708e48e
zengfeng75 <zengfeng75@gmail.com>
2020-03-05 15:03:21 +0800

    no message

--- _learn_jenkins/start.cmd
+++ _learn_jenkins/start.cmd
@@ -1,4 +1,4 @@
 set JENKINS_HOME=%cd%/.jenkins
 echo %JENKINS_HOME%
-java -jar jenkins.war --httpPort=9090
+java -Dfile.encoding=UTF-8 -Dsun.jnu.encoding=UTF-8 -jar jenkins.war --httpPort=9090
 pause
